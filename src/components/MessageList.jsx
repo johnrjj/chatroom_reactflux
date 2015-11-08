8609c4bc279d59c15bbdce2c06bd0553aec2eb1d
John Johnson <johnrjj@gmail.com>
2015-11-08 18:20:16 -0500

    CR-009 Added in firebase

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Message from './Message.jsx'
 import mui from 'material-ui';
+import Firebase from 'firebase';
+import _ from 'lodash';
 
 // Object destructuring!!!!
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
@@ -11,22 +13,35 @@ class MessageList extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      messages: [
-        'message1',
-        'message2'
-      ]
+      messages: []
     };
-  }
 
+    this.firebaseRef = new Firebase('https://reactchatswag.firebaseio.com/messages');
+    this.firebaseRef.once('value', (DataSnapshot) => {
+
+      var messageVal = DataSnapshot.val();
+      var messages = _(messageVal)
+        .keys() //array of all keys in obj
+        .map((messageKey) => {
+          var cloned = _.clone(messageVal[messageKey]);
+          cloned.key = messageKey; // Add a key to make react happy
+          return cloned;
+        })
+        .value(); // run da chain
+      console.log(messages);
+      this.setState({
+        messages: messages
+      });
+    });
+  }
 
   render() {
     var messageNodes = this.state.messages.map((message) => {
       return (
-        <Message message={message} />
+        <Message message={message.message}  key={message.key}/>
       );
     });
 
-    // render should only return one element, hence wrapping messagenodes
     return (
       <Card style={{
           flexGrow: 2,
