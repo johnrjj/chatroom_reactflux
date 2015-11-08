7eb2cd9a1f9bb7552b8def17f77bb57639a95b9d
John Johnson <johnrjj@gmail.com>
2015-11-08 17:12:03 -0500

    CR-008 Added testing support via jest

---
+++ __tests__/sampletest.js
@@ -0,0 +1,10 @@
+// __tests__/sum-test.js
+// jest.dontMock('../fileNotToMock');
+
+describe('TestSuite', () => {
+  it('sample test description', () => {
+    var i = 1;
+    var j = 2;
+    expect(i+j).toEqual(3);
+  });
+});
